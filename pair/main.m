fd34c8834e8593f208d2bac32b13664f66c93553
Dave Dribin <dave@dribin.org>
2012-04-17 07:39:04 -0500

    Initial Commit

---
+++ pair/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  pair
+//
+//  Created by Dave Dribin on 4/17/12.
+//  Copyright (c) 2012 Bit Maki, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
